bc193885a78bb5656651a311934809da26154e7a
kanozec <kanozec@gmail.com>
2018-11-02 02:30:10 +0800

    Fix the typo

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
+	router.Run(":5005")
 }
