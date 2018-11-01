8499751666d3f178bfc464a156e895b0fac4ed2a
kanozec <kanozec@gmail.com>
2018-11-02 02:47:41 +0800

    Add the db connection

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func main() {
+	db, err := gorm.Open("sqlite3", "./../gophergram.db")
+	if err != nil {
+		fmt.Println("Initialize db connection error", err)
+	}
+	db.DB().SetMaxIdleConns(10)
+	defer db.Close()
+
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World")
