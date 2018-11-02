40969ab29bf40ecac3f54d9077e94867333168b5
kanozec <kanozec@gmail.com>
2018-11-02 10:32:46 +0800

    Load config from .env

--- main.go
+++ main.go
@@ -4,12 +4,29 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/kanozec/gophergram/model"
 )
 
+type config struct {
+	ServerPort string `env:"SERVERPORT" envDefault:":8080"`
+}
+
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("File .env not found, using the default value")
+	}
+
+	var cfg config
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Println("Failed to parse ENV")
+	}
+
 	db, err := model.InitializeDB()
 	if err != nil {
 		fmt.Println("Initalize db error")
@@ -19,5 +36,5 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World")
 	})
-	router.Run(":5005")
+	router.Run(cfg.ServerPort)
 }
