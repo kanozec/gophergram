d9b3ce97b35f272ff783f45b10aa07f8972789b0
kanozec <kanozec@gmail.com>
2018-11-03 18:21:34 +0800

    Modify some view

--- controllers/static.go
+++ controllers/static.go
@@ -13,6 +13,16 @@ func NewStatic() *Static {
 	return &Static{}
 }
 
+type article struct {
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
 func (s *Static) Contact(c *gin.Context) {
-	c.HTML(http.StatusOK, "type", gin.H{})
+	var articleList = []article{
+		article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
+		article{ID: 2, Title: "Article 2", Content: "Article 2 body"},
+	}
+	c.HTML(http.StatusOK, "type", gin.H{"payload": articleList})
 }
